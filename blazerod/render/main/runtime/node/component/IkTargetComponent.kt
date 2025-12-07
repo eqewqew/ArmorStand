6f36ed579e95ca49ddb0ff2adf11227bc1fb3a7a
eqewqew <101486244+eqewqew@users.noreply.github.com>
2025-12-07 01:47:22 -0800

    Update IkTargetComponent.kt

--- blazerod/render/main/runtime/node/component/IkTargetComponent.kt
+++ blazerod/render/main/runtime/node/component/IkTargetComponent.kt
@@ -184,11 +184,20 @@ class IkTargetComponent(
 
             val chainIkPos = ikPos.mulPosition(invChain, chainIkPos)
             val chainTargetPos = targetPos.mulPosition(invChain, chainTargetPos)
-
+            val chainIkPos = ikPos.mulPosition(invChain, Vector3())
+            val chainTargetPos = targetPos.mulPosition(invChain, Vector3())
             // Unnormalized vector seems never used then, so directly overwrite them
-            val chainIkVec = chainIkPos.normalize()
-            val chainTargetVec = chainTargetPos.normalize()
-
+            val chainIkVec = if (chainIkPos.lengthSquared() > 1e-8f) {
+            chainIkPos.normalize()
+            } else {
+            continue
+            }
+            val chainTargetVec = if (chainTargetPos.lengthSquared() > 1e-8f) {
+            chainTargetPos.normalize()
+            } else {
+           continue
+            }
+            
             val dot = chainTargetVec.dot(chainIkVec).coerceIn(-1f, 1f)
 
             var angle = acos(dot)
@@ -197,7 +206,13 @@ class IkTargetComponent(
                 continue
             }
             angle = angle.coerceIn(-limitRadian, limitRadian)
-            val cross = chainTargetVec.cross(chainIkVec, cross).normalize()
+
+            val crossRaw = chainTargetVec.cross(chainIkVec, Vector3())
+            val cross = if (crossRaw.lengthSquared() > 1e-8f) {
+            crossRaw.normalize()
+            } else {
+            continue
+            }
             val rot = rot.rotationAxis(angle, cross)
 
             val chainRot = instance.getTransformMap(chain.nodeIndex)
@@ -304,6 +319,7 @@ class IkTargetComponent(
         node: RenderNodeImpl,
         instance: ModelInstanceImpl,
     ) {
+        val epsilon=1e-6f
         val enabled = instance.modelData.ikEnabled[ikIndex]
         if (!enabled) {
             return
@@ -331,26 +347,24 @@ class IkTargetComponent(
                     // We use distanceSquared() here, unlike original code
                     val dist = targetPos.distanceSquared(ikPos)
 
-                    if (dist < maxDist) {
+                    if (dist < maxDist-epsilon) {
                         maxDist = dist
                         for (chain in chains) {
                             val matrix = instance.getTransformMap(chain.nodeIndex).get(transformId)
                             if (matrix != null) {
                                 matrix.getRotation(chain.saveIKRot)
-                            } else {
-                                chain.saveIKRot.identity()
-                            }
+                            }  else {
+                            chain.saveIKRot.identity()
                         }
-                    } else {
-                        for (chain in chains) {
+                        }
+                    } 
+                    }
+                }
+            for (chain in chains) {
                             instance.setTransformDecomposed(chain.nodeIndex, transformId) {
                                 rotation.set(chain.saveIKRot)
                             }
-                        }
-                        instance.updateNodeTransform(chains.last().nodeIndex)
-                        break
-                    }
-                }
+                instance.updateNodeTransform(chains.last().nodeIndex)
             }
 
             is UpdatePhase.DebugRender -> {
